aade753cdaf6a3feaf57bbf1d5a266f157959f15
инет <@KPC.RU>
2022-02-01 08:38:07 +0300

    LR1

---
+++ DLibrary/MallardDuck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Laborator1
+{
+    
+    public class MallardDuck : Duck
+    {
+        public override string display()
+        {
+            return "display" + this.GetType();
+        }
+    }
+}
---
+++ WoundedDuck/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Laborator1;
+
+namespace WoundedDuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedheadDuck rDuck = new RedheadDuck();
+            MallardDuck mDuck = new MallardDuck();
+
+            Duck[] duck = { rDuck, mDuck };
+
+            foreach (Duck Duck in duck)
+            {
+                Console.WriteLine(Duck.display());
+                Console.WriteLine(Duck.quack());
+                Console.WriteLine(Duck.swim());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
